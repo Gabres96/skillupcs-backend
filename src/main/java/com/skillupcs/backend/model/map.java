70765bf36d7fd3f08d6227ef614406bd978bcce2
Gabriel Elias <gabriel.rodrigues670@gmail.com>
2025-10-29 21:10:35 -0300

    feat: setup inicial do projeto backend, docker-compose e entidade Map

---
+++ src/main/java/com/skillupcs/backend/model/map.java
@@ -0,0 +1,28 @@
+package com.skillupcs.backend.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.aspectj.weaver.Position;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class map {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "map", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Position> positions;
+
+}
